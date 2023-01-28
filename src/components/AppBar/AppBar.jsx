f84afc3bcc5c03e83e040888ab0cd588e657db6f
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-01-28 22:32:53 +0200

    added search input

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,6 +4,8 @@ import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks';
 
 import { AppBar, Toolbar, Box } from '@mui/material';
+import { Search, SearchIconWrapper, StyledInputBase } from './AppBar.styled';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const AppBarComponent = () => {
   const { isLoggedIn } = useAuth();
@@ -12,6 +14,18 @@ export const AppBarComponent = () => {
     <AppBar>
       <Toolbar>
         <Navigation />
+        {isLoggedIn && (
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Find contacts by name"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        )}
+
         <Box sx={{ ml: 'auto' }}>{isLoggedIn ? <UserMenu /> : <AuthNav />}</Box>
       </Toolbar>
     </AppBar>
